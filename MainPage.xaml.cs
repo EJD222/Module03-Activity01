e8a96856eb635649d2658ba127ee26fa33a3f8d0
EL-J Diaz <91679071+EJD222@users.noreply.github.com>
2024-02-23 15:36:16 +0800

    Add Module03-Activity01 files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ImagePage_Open(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ImagePage());
+        }
+
+        private async void StackLayout_Activity(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity2());
+        }
+
+        private async void Grid_Activity(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity3());
+        }
+    }
+}
